archdx/zerolog-sentry

commit 3f53baa3c58d64eca2db6317a479a17fb27d8184

add breadcrumbs support

writer.go
@@ -30,8 +30,31 @@ var now = time.Now
 type Writer struct {
 	hub *sentry.Hub
 
-	levels       map[zerolog.Level]struct{}
-	flushTimeout time.Duration
+	levels          map[zerolog.Level]struct{}
+	flushTimeout    time.Duration
+	withBreadcrumbs bool
+}
+
+// addBreadcrumb adds event as a breadcrumb
+func (w *Writer) addBreadcrumb(event *sentry.Event) {
+	if !w.withBreadcrumbs {
+		return
+	}
+
+	// category is totally optional, but it's nice to have
+	var category string
+	if _, ok := event.Extra["category"]; ok {
+		if v, ok := event.Extra["category"].(string); ok {
+			category = v
+		}
+	}
+
+	w.hub.AddBreadcrumb(&sentry.Breadcrumb{
+		Category: category,
+		Message:  event.Message,
+		Level:    event.Level,
+		Data:     event.Extra,
+	}, nil)
 }
 
 // Write handles zerolog's json and sends events to sentry.
@@ -43,19 +66,23 @@ func (w *Writer) Write(data []byte) (n int, err error) {
 		return n, nil
 	}
 
-	if _, enabled := w.levels[lvl]; !enabled {
-		return
-	}
-
 	event, ok := w.parseLogEvent(data)
 	event.Level = levelsMapping[lvl]
 
-	if ok {
-		w.hub.CaptureEvent(event)
-		// should flush before os.Exit
-		if event.Level == sentry.LevelFatal {
-			w.hub.Flush(w.flushTimeout)
-		}
+	if !ok {
+		return
+	}
+
+	if _, enabled := w.levels[lvl]; !enabled {
+		// if the level is not enabled, add event as a breadcrumb
+		w.addBreadcrumb(event)
+		return
+	}
+
+	w.hub.CaptureEvent(event)
+	// should flush before os.Exit
+	if event.Level == sentry.LevelFatal {
+		w.hub.Flush(w.flushTimeout)
 	}
 
 	return
@@ -188,6 +215,7 @@ type config struct {
 	environment      string
 	serverName       string
 	ignoreErrors     []string
+	breadcrumbs      bool
 	debug            bool
 	tracing          bool
 	debugWriter      io.Writer
@@ -242,6 +270,13 @@ func WithServerName(serverName string) WriterOption {
 func WithIgnoreErrors(reList []string) WriterOption {
 	return optionFunc(func(cfg *config) {
 		cfg.ignoreErrors = reList
+	})
+}
+
+// WithBreadcrumbs enables sentry client breadcrumbs.
+func WithBreadcrumbs() WriterOption {
+	return optionFunc(func(cfg *config) {
+		cfg.breadcrumbs = true
 	})
 }
 
@@ -350,9 +385,10 @@ func New(dsn string, opts ...WriterOption) (*Writer, error) {
 	}
 
 	return &Writer{
-		hub:          sentry.CurrentHub(),
-		levels:       levels,
-		flushTimeout: cfg.flushTimeout,
+		hub:             sentry.CurrentHub(),
+		levels:          levels,
+		flushTimeout:    cfg.flushTimeout,
+		withBreadcrumbs: cfg.breadcrumbs,
 	}, nil
 }
 

commit d4254603aef570666ea2619ed4aad72056dbb259

Fixes a panic when Trace level log fired
This was a minor issue introduced in 3f53baa3c58d64eca2db6317a479a17fb27d8184 
The issue is minor, because Trace should not be enabled in most prod deployments, but still bit nasty :)

writer.go
@@ -67,8 +67,10 @@ func (w *Writer) Write(data []byte) (n int, err error) {
 	}
 
 	event, ok := w.parseLogEvent(data)
-	event.Level = levelsMapping[lvl]
-
+	if !ok {
+		return
+	}
+	event.Level, ok = levelsMapping[lvl]
 	if !ok {
 		return
 	}

writer_test.go
@@ -79,6 +79,29 @@ func TestWrite(t *testing.T) {
 	require.True(t, beforeSendCalled)
 }
 
+func TestWrite_TraceDoesNotPanic(t *testing.T) {
+	beforeSendCalled := false
+	writer, err := New("", WithBeforeSend(func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event {
+		beforeSendCalled = true
+		return event
+	}))
+	require.Nil(t, err)
+
+	var zerologError error
+	zerolog.ErrorHandler = func(err error) {
+		zerologError = err
+	}
+
+	// use io.MultiWriter to enforce using the Write() method
+	log := zerolog.New(io.MultiWriter(writer)).With().Timestamp().
+		Str("requestId", "bee07485-2485-4f64-99e1-d10165884ca7").
+		Logger()
+	log.Trace().Msg("test message")
+
+	require.Nil(t, zerologError)
+	require.False(t, beforeSendCalled)
+}
+
 func TestWriteLevel(t *testing.T) {
 	beforeSendCalled := false
 	writer, err := New("", WithBeforeSend(func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event {